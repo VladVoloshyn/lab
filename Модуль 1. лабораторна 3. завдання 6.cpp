23ed56276c7022fa49fc8d3c66f39deacb04f29a
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2021-10-26 13:14:54 +0300

    Add files via upload

---
+++ Модуль 1. лабораторна 3. завдання 6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x)
+{
+	return pow(x, 2); // беремо функцію y = x2 і записуємо її в функцію f.
+}
+
+int main()
+{
+	const int N = 1000; // розбиваємо відрізок а і b на 1000 частин.
+
+	double a, b;
+	cout << "a = "; // початкова точка.
+	cin >> a;
+	cout << "b = "; // кінцева точка.
+	cin >> b;
+	double sum = 0;
+	double dx = (b - a) / N; // dx інтервал по осі OX.
+	for (int i = 0; i < N; ++i)
+	{
+		double y1 = f(a + i * dx); // для кожного x шукаємо свій y по формулам.
+		double y2 = f(a + (i + 1) * dx);
+		sum += sqrt((dx * dx) + pow(y2 - y1, 2)); // сума шукаємо по формулі
+	}
+	cout << " L=" << sum;
+	return 0;
+}
