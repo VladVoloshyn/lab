4a91bfb96c9a93cf7b27860668354c724c87d7cb
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2021-11-12 10:54:38 +0200

    Add files via upload

---
+++ лабораторна 4. модуль 1.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <conio.h>
+# include <iostream>
+# include <windows.h>
+# include <iomanip>
+# include <fstream>
+# include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int i, j, c;
+	const int N = 4;
+	int a[N];
+	for (i = 0; i < N; i++)
+	{
+		a[i] = i + 1;
+	}
+
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N - 1; j++)
+		{
+			c = 0;
+			int temp = a[j];
+			a[j] = a[j + 1];
+			a[j + 1] = temp;// Цикл який міняє місцями цифри
+
+			for (int z = 0; z < N; ++z)
+			{
+				if (a[z] == z + 1)
+					c = 1;
+			}
+
+
+			for (int z = 0; z < N; ++z)
+			{
+				if (c == 0)
+					cout << a[z] << " ";
+				else break;
+			}
+			cout << '\n';
+		}
+	}
+
+
+
+
+	return(0);
+}
+
