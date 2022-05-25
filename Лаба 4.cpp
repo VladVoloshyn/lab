444ba61341e3d46ac68a3c6729521d94a9bde6c2
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2022-05-25 09:12:51 +0300

    Add files via upload

---
+++ Лаба 4.cpp
@@ -0,0 +1,232 @@
+﻿//Реалізувати перевід рядка у expression tree(змінні, дійсні числа, операції + , -
+	//       , *, / , унарний мінус, степінь. Обчислити вираз, роблячи запит по змінним
+#include<iostream>
+#include<stack>
+#include<string>
+#pragma warning(disable : 4996) 
+using namespace std;
+
+int prec(char c) {
+	if (c == '^')
+		return 3;
+	else if (c == '/' || c == '*')
+		return 2;
+	else if (c == '+' || c == '-')
+		return 1;
+	else
+		return -1;
+}
+
+
+string infixToPostfix(string s) 
+{
+	stack<char> st;
+	string result;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		char c = s[i];
+		
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+			result += c;
+		
+		else 
+			if (c == '(')
+			st.push('(');
+		    else if (c == ')')
+					{
+					while (st.top() != '(')
+					{
+					result += st.top();
+					st.pop();
+					}
+					st.pop();
+					}
+			     else 
+					 {
+					 while (!st.empty() && prec(s[i]) <= prec(st.top())) 
+						{
+						result += st.top();
+						st.pop();
+						}
+						st.push(c);
+					 }
+	}
+
+
+	while (!st.empty()) 
+	{
+		result += st.top();
+		st.pop();
+	}
+
+	return result;
+}
+
+
+struct Node
+{
+	string val;
+	Node* left, * right;
+
+	Node(string val)
+	{
+		this->val = val;
+		this->left = this->right = nullptr;
+	}
+};
+
+
+bool isLeaf(Node * node) {
+	return node->left == nullptr && node->right == nullptr;
+}
+
+
+double process(string op, double x, double y)
+{
+	if (op == "+") { return x + y; }
+	if (op == "-") { return x - y; }
+	if (op == "*") { return x * y; }
+	if (op == "/") { return x / y; }
+	if (op == "^") { return pow(x, y); }
+	return 0;
+}
+
+
+double evaluate(Node * root)
+{
+
+	if (root == nullptr) 
+	{
+		return 0;
+	}
+
+
+	if (isLeaf(root)) 
+	{
+		return stod(root->val);
+	}
+
+
+	double x = evaluate(root->left);
+	double y = evaluate(root->right);
+
+
+	return process(root->val, x, y);
+}
+
+
+void evaluate_tree(string postfix)  
+{
+	Node* root = new Node(&postfix[postfix.size() - 1]);    
+	Node * root2 = root;
+	char x; char x_arr[2];
+	x_arr[1] = '\0';
+	int check = 0;
+	for (int i = 0; i < postfix.size() - 1; i++)
+	{
+		if (!isdigit(postfix[i]))
+		{
+			while (check != 1)
+			{
+				if (root2->left == NULL)  
+				{
+					x_arr[0] = postfix[i];
+					root2->left = new Node(&x_arr[0]);
+					check = 1;
+				}
+				else if (root2->right == NULL)
+				{
+					x_arr[0] = postfix[i];
+					root2->right = new Node(&x_arr[0]);
+					check = 1;
+				}
+				else
+				{
+					root2 = root2->right;
+				}
+			}
+			check = 0;
+		}
+
+	}
+	root2 = root;
+	for (int i = 0; i < postfix.size() - 1; i++)
+	{
+		if (isdigit(postfix[i]))
+		{
+			while (check != 1)
+			{
+				if (root2->left != NULL) {  //
+					strcpy(&x_arr[0], root2->left->val.c_str()); // ВКАЗІВНИК НА СИМВОЛ left копіює дані в &x_arr
+				}
+				if (root2->left == NULL)
+				{
+
+					x_arr[0] = postfix[i];
+					root2->left = new Node(&x_arr[0]);
+					check = 1;
+				}
+
+				else if (root2->left != NULL && !isdigit(x_arr[0]))
+				{
+
+					root2 = root2->left;
+				}
+				else if (root2->right == NULL)
+				{
+
+					x_arr[0] = postfix[i];
+					root2->right = new Node(&x_arr[0]);
+					check = 1;
+				}
+				else
+				{
+					strcpy(&x_arr[0], root2->left->val.c_str());
+					if (!isdigit(x_arr[0]))
+					{
+						root2 = root2->left;
+					}
+					else
+					{
+						strcpy(&x_arr[0], root2->right->val.c_str());
+						if (isdigit(x_arr[0]))
+						{
+							if (root->right == NULL)
+							{
+								x_arr[0] = postfix[i];
+								root->right = new Node(&x_arr[0]);
+								check = 1;
+							}
+							else { 
+								root2 = root->right; 
+							}
+
+
+						}
+
+					}
+				}
+			}
+			check = 0;
+		}
+
+	}
+
+	cout << endl << endl << "Result: " << evaluate(root) << endl;
+}
+
+
+
+
+int main() {
+	string exp = "3^2*(1+2)";
+
+	cout << exp;
+	string res = infixToPostfix(exp);
+
+
+	evaluate_tree(res);
+	cout << res << endl;
+	return 0;
+}
--- Лаба 6.cpp
+++ Лаба 6.cpp
@@ -1,73 +1,170 @@
-﻿#include <iostream>
+﻿//Визначити хроматичний многочлен та хроматичне число графа.
+
+
+#include<iostream>
+#include<cmath>
 #include <fstream>
-#include <stack>
+
 using namespace std;
-int main()
+
+enum Type
+{
+	full, path, cycle
+};
+
+struct Graf
 {
 
-	string path = "myFile.txt";
-	string str;
-	stack<char> oper;
-	stack<long int> num;
-	char ch;
-	long int temp = 0, res = 0;
-	bool first = 1;
-	ifstream fs;
-	fs.open(path, fstream::out);
-	if (!fs.is_open())
+	int** mat;
+	int type;
+	int count_tops;
+
+	Graf()
 	{
-		cout << "Error" << endl;
+		this->count_tops = 0;
+	}
+
+	Graf(int count_tops, int type)
+	{
+		mat = new int* [count_tops];
+		for (int i = 0; i < count_tops; ++i)
+			mat[i] = new int[count_tops];
+
+		this->type = type;
+		this->count_tops = count_tops;
+	}
+
+};
+
+
+
+void fill()
+{
+	ofstream out("Graf.txt", ios::binary | ios::app);
+	Graf Mas(3, 0);
+	out.write((char*)& Mas.count_tops, sizeof(Mas.count_tops));
+	out.write((char*)& Mas.type, sizeof(Mas.type));
+
+	Mas.mat[0][0] = 0;
+	Mas.mat[0][1] = 1;
+	Mas.mat[0][2] = 0;
+	Mas.mat[1][0] = 1;
+	Mas.mat[1][1] = 0;
+	Mas.mat[1][2] = 1;
+	Mas.mat[2][0] = 1;
+	Mas.mat[2][1] = 1;
+	Mas.mat[2][2] = 0;
+
+	for (int i = 0; i < Mas.count_tops; i++)
+	{
+		for (int j = 0; j < Mas.count_tops; j++)
+		{
+			out.write((char*)& Mas.mat[i][j], sizeof(Mas.mat[i][j]));
+		}
+	}
+	out.close();
+}
+
+float ch_polu_full(int x, int k, int n)
+{
+	float poly = 1;
+	if (x < k)
+	{
 		return 0;
 	}
-	else
+
+	for (int i = 1; i <= n; i++)
 	{
-		cout << "Success" << endl;
-		while (!fs.eof())
+		poly = poly * (x - i + 1);
+	}
+	return poly;
+}
+
+
+float ch_polu_path(int x, int k, int n)
+{
+	return x * pow((x - 1), n - 1);
+}
+
+
+float ch_polu_cycle(int x, int k, int n)
+{
+
+	return pow((x - 1), n) + pow((-1), n) * (x - 1);
+}
+
+
+void lab()
+{
+	int g1 = 0;
+	int type;
+	int read;
+	ifstream in("Graf.txt", ios::binary);
+
+	while (!in.eof())
+	{
+		in.read((char*)& g1, sizeof(g1));
+		in.read((char*)& type, sizeof(type));
+		Graf g(g1, type);
+
+		for (int i = 0; i < g1; i++)
 		{
-			ch = fs.peek();
-			if (first)
+			for (int j = 0; j < g1; j++)
 			{
-				fs >> res;
-				num.push(temp);
-				cout << res;
-				first = 0;
-				continue;
-			}
-			if (ch >= 48 && ch <= 57)
-			{
-				fs >> temp;
-				num.push(temp);
-				cout << temp;
-				continue;
-			}
-			if (ch == '+')
-			{
-				cout << ch;
-				oper.push(ch);
-				fs.ignore();
-				continue;
+				in.read((char*)& read, sizeof(read));
+				g.mat[i][j] = read;
 			}
 		}
-		while (oper.size() != 0)
+
+
+
+		int x;
+		cout << "Введите кiлькiсть кольорiв" << endl;
+		cin >> x;
+		int ch_num = 0;
+
+		for (int i = 0; i < g.count_tops; i++)
 		{
-			long int a;
-			a = num.top();
-			num.pop();
-			ch = oper.top();
-			oper.pop();
-			if (ch == '+')
+			for (int j = 0; j < g.count_tops; j++)
 			{
-				res += a;
-				continue;
+				if (g.mat[i][j] == 0 && i != j)
+				{
+					ch_num++;
+				}
 			}
-			if (ch == '-')
-			{
-				res -= a;
-				continue;
-			}
+		}
+
+		ch_num /= 2;
+		if (ch_num == 0 && g.count_tops != 0)
+		{
+			ch_num = g.count_tops;
+		}
+		cout << "Хроматичне число графа = " << ch_num << endl;
+
+
+		switch (g.type)
+		{
+		case full:
+			cout << "Хроматичний многочлен графа = " << ch_polu_full(x, ch_num, g.count_tops) << endl;
+			break;
+		case path:
+			cout << "Хроматичний многочлен графа = " << ch_polu_path(x, ch_num, g.count_tops) << endl;
+			break;
+		case cycle:
+			cout << "Хроматичний многочлен графа = " << ch_polu_cycle(x, ch_num, g.count_tops) << endl;
+			break;
+		default:
+			break;
 		}
 	}
-	cout << "\nresult = " << res;
-	fs.close();
-	cout << "\nclosing" << endl;
+	in.close();
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	fill();
+	lab();
+	system("pause");
+	return 0;
 }
